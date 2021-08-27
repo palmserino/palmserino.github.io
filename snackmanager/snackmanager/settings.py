dab06ac4b95dfb5ccf76f66165d2af1552bca16f
palmserino <57275266+palmserino@users.noreply.github.com>
2021-08-26 17:41:05 -0700

    Fix for project name change

--- snackmanager/snackmanager/settings.py
+++ snackmanager/snackmanager/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'snackmanager.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'snackmanager.wsgi.application'
 
 
 # Database
