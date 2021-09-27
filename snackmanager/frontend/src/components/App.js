02a7a1c62caa74ee3e12f3f05e7e444b233e2a4a
Carter Peyton <carterpeyton@pop-os.localdomain>
2021-09-27 15:03:18 -0700

    Added register and login frontend pages

--- snackmanager/frontend/src/components/App.js
+++ snackmanager/frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import { Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -7,6 +8,8 @@ import AlertTemplate from 'react-alert-template-basic';
 import Header from './layout/Header';
 import Dashboard from './snacks/Dashboard';
 import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
 
 import { Provider } from 'react-redux'; // provider connects react to redux 
 import store from '../store'; 
@@ -22,13 +25,20 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <Fragment>
-                        <Header />
-                        <Alerts />
-                        <div className='container'>
-                            <Dashboard />
-                        </div>
-                    </Fragment>  
+                    <Router>
+                        <Fragment>
+                            <Header />
+                            <Alerts />
+                            <div className='container'>
+                                <Switch>
+                                    <Route exact path="/" component={Dashboard} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                </Switch>
+                                
+                            </div>
+                        </Fragment>  
+                    </Router>
                 </AlertProvider>
             </Provider>
         )
--- snackmanager/frontend/src/components/layout/Header.js
+++ snackmanager/frontend/src/components/layout/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 /* Creates a nav bar */
 
@@ -19,6 +20,13 @@ export class Header extends Component {
                         <li className="nav-item">
                         <a className="nav-link active" aria-current="page" href="/admin">Admin</a>
                         </li>
+                        <li className="nav-item">
+                        <Link to="/register" className="nav-link">Register</Link>
+                        </li>
+                        <li className="nav-item">
+                        <Link to="/login" className="nav-link">Login</Link>
+                        </li>
+                        
                     </ul>
                     </div>
                 </div>
