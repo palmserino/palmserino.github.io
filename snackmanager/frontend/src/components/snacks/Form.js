7ea24b98af22d16ac0c3fca46556b6af16405bc0
Carter Peyton <carterpeyton@pop-os.localdomain>
2021-10-08 17:16:56 -0700

    form in proper grid style

--- snackmanager/frontend/src/components/snacks/Form.js
+++ snackmanager/frontend/src/components/snacks/Form.js
@@ -56,81 +56,93 @@ export class Form extends Component {
         const { name, price, store_name, quantity, type, total_cals, time_purchased } = this.state;
         return (
             <div className="card card-body mt-4 mb-4">
-                <h2>Add Snack</h2>
+                <h2 className="d-flex justify-content-center">Add Snack Purchase</h2>
                 <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input
-                    className="form-control"
-                    type="text"
-                    name="name"
-                    onChange={this.onChange}
-                    value={name}
-                    />
+                <div className="row mt-2">
+                    <div className="col">
+                        <label>Name</label>
+                        <input
+                        className="form-control"
+                        type="text"
+                        name="name"
+                        onChange={this.onChange}
+                        value={name}
+                        />
+                    </div>
+                    <div className="col">
+                        <label>Type</label>
+                        <Select
+                            name="type"
+                            value={option.find(item => item.value === type)}
+                            onChange={this.changeHandler}
+                            options={option}
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label>Price</label>
-                    <input
-                    className="form-control"
-                    type="text"
-                    name="price"
-                    onChange={this.onChange}
-                    value={price}
-                    />
+
+                <div className="row mt-2">
+                    <div className="col">
+                            <label>Quantity</label>
+                            <input
+                            className="form-control"
+                            type="number"
+                            name="quantity"
+                            onChange={this.onChange}
+                            value={quantity}
+                            />
+                    </div>
+                    <div className="col">
+                        <label>Price / unit</label>
+                        <input
+                        className="form-control"
+                        type="text"
+                        name="price"
+                        onChange={this.onChange}
+                        value={price}
+                        />
+                    </div>
+                    <div className="col">
+                        <label>Calories / unit</label>
+                        <input
+                        className="form-control"
+                        type="number"
+                        name="total_cals"
+                        onChange={this.onChange}
+                        value={total_cals}
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label>Where did you buy this snack?</label>
-                    <input
-                    className="form-control"
-                    type="text"
-                    name="store_name"
-                    onChange={this.onChange}
-                    value={store_name}
-                    />
+
+                <div className="row mt-2">
+                    <div className="col">
+                        <label >Where did you buy this snack?</label>
+                        <input
+                        className="form-control "
+                        type="text"
+                        name="store_name"
+                        onChange={this.onChange}
+                        value={store_name}
+                        />
+                    </div>
+
+                    <div className="col">
+                        <label>Time Purchased</label>
+                        <input
+                        className="form-control"
+                        type="datetime-local"
+                        name="time_purchased"
+                        onChange={this.onChange}
+                        value={time_purchased}
+                        />
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label>Quantity</label>
-                    <input
-                    className="form-control"
-                    type="number"
-                    name="quantity"
-                    onChange={this.onChange}
-                    value={quantity}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Type</label>
-                    <Select
-                        name="type"
-                        value={option.find(item => item.value === type)}
-                        onChange={this.changeHandler}
-                        options={option}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Total Calories</label>
-                    <input
-                    className="form-control"
-                    type="number"
-                    name="total_cals"
-                    onChange={this.onChange}
-                    value={total_cals}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Time Purchased</label>
-                    <input
-                    className="form-control"
-                    type="datetime-local"
-                    name="time_purchased"
-                    onChange={this.onChange}
-                    value={time_purchased}
-                    />
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary">
-                    Submit
-                    </button>
+
+                <div className="row mt-4">
+                    <div className="col d-flex justify-content-center">
+                        <button type="submit" className="btn btn-primary">
+                        Submit
+                        </button>
+                    </div>
                 </div>
                 </form>
             </div>
