bddf6789bdd07fa154ce970de7e36976e49a87e2
Carter Peyton <carterpeyton@pop-os.localdomain>
2021-10-10 14:58:39 -0700

    routes init

--- snackmanager/frontend/src/components/App.js
+++ snackmanager/frontend/src/components/App.js
@@ -41,6 +41,7 @@ class App extends Component {
                                     <PrivateRoute exact path="/" component={Dashboard} />
                                     <Route exact path="/register" component={Register} />
                                     <Route exact path="/login" component={Login} />
+                                    <Route exact path="/eat" component={Login} />
                                 </Switch>
                                 
                             </div>
