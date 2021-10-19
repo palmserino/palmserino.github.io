5cb63b5a398dfdeee6d4109c621a61041322e74f
Carter Peyton <carterpeyton@pop-os.localdomain>
2021-10-18 22:48:22 -0700

    initial serializer and api setup for eat model

--- snackmanager/snacks/serializers.py
+++ snackmanager/snacks/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers 
-from .models import Snack
+from .models import Snack, Eat 
+
+# use django rest_framework to serializer model objects that 
+# will eventually get turned into JSON 
 
 class SnackSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snack
+        fields = '__all__'
+
+class EatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Eat
         fields = '__all__'
